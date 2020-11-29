80a62ab2c1d2d2928974ef3812fc58a81caacf4b
Renddi Yeh <renddi@relab.cc>
2020-11-29 18:22:03 +0800

    added cover

--- src/Bar.js
+++ src/Bar.js
@@ -2,6 +2,9 @@ import React, { useMemo } from "react";
 import { Bar as VxBar } from "@vx/shape";
 import { Text as VxText } from "@vx/text";
 import { RectClipPath } from '@vx/clip-path'
+import { Group } from '@vx/group'
+
+import { ReactComponent as Bang } from './bang.svg'
 
 // import { format } from 'd3-format'
 
@@ -48,9 +51,9 @@ const Bar = ({ color, x, y, width, height, name, value, author, id, xMax }) => {
       <VxText x={x + lableX - 10} y={y + 8 + height / 2} textAnchor="end" fontWeight="700" fontFamily="Noto Sans TC" fontSize="24" clipPath={`url(#clip-${id})`}>
         {name}
       </VxText>
-      <VxText x={x + lableX + 10} y={y + 8 + height / 2} fontFamily="Noto Sans TC" fontSize="24" fontWeight="700">
-        {value > 20000 ? '爆！' : ''}
-      </VxText>
+      <Group left={x + lableX + 10} top={y + 2}>
+        {value > 21000 ? <Bang /> : null}
+      </Group>
     </React.Fragment>
   );
 };
--- src/RacingBarChart.js
+++ src/RacingBarChart.js
@@ -8,6 +8,10 @@ import RacingAxisTop from "./RacingAxisTop";
 import RacingBarGroup from "./RacingBarGroup";
 import { ReactComponent as Author } from './author.svg'
 import { ReactComponent as Trophy } from './trophy.svg'
+import { ReactComponent as Champ } from './champ.svg'
+
+import imgs from './imgs'
+import { nth } from "lodash";
 
 const categories = ['文學小說', '商業理財', '生活旅遊', '心理勵志', '其他']
 
@@ -126,7 +130,10 @@ const RacingBarChart = React.forwardRef(({
       }),
     [numOfBars, yMax]
   );
-  const quarter = `Q${Math.floor(currentDate.getMonth() / 3) + 1}`;
+  const y = currentDate.getFullYear()
+  const q = Math.floor(currentDate.getMonth() / 3)
+  const quarter = `Q${q + 1}`;
+  const nthQ = (y - 2010) * 4 + q - 3
   // const legendBandwidth = lengendScale.bandwidth()
   return (
     <svg style={{ width: '100%', border: '1px solid', background: '#f2f2f2' }} viewBox={`0 0 ${width} ${height}`}>
@@ -188,70 +195,35 @@ const RacingBarChart = React.forwardRef(({
           xMax={xMax}
           ref={barGroupRef}
         />
-        <text
-          textAnchor="end"
-          fontSize="64"
-          x={xMax}
-          y={yMax - 54}
-          fontWeight="700"
-          fontFamily="Noto Sans TC"
-          fill="#333333"
-        >
-          {currentDate.getFullYear()}年
-          <tspan dx="-10" fontSize="120" fill="#ffc027" stroke="black" strokeWidth="4"> {quarter}</tspan>
-        </text>
-        <line
-          x1={xMax - 400}
-          y1={yMax - 20}
-          x2={xMax}
-          y2={yMax - 20}
-          stroke="#b3b3b3"
-          strokeWidth="3"
-          strokeDasharray="16"
-          strokeLinecap="round"
-        ></line>
-        {/* <Group left={xMax - 120} top={yMax - 30}>
-        <g>
-          <path d="M9.25,6.48,7.32,3.59,5.39.69a.64.64,0,0,0-1,0L2.44,3.59.52,6.48c-.23.34,0,.77.51.77H3.67V27.52a1.21,1.21,0,1,0,2.42,0V7.25H8.74A.52.52,0,0,0,9.25,6.48Z" fill="#f2f2f2" stroke="#1a1a1a" stroke-linecap="round" stroke-linejoin="round" stroke-width="0.87"/>
-          <g>
-            <rect x="22.35" y="2.35" width="17.25" height="21.28" stroke-width="1.15" stroke="#1a1a1a" stroke-linecap="round" stroke-linejoin="round" fill="#fff"/>
-            <rect x="18.9" y="5.23" width="17.25" height="21.28" stroke-width="0.86" stroke="#1a1a1a" stroke-linecap="round" stroke-linejoin="round" fill="#fbb03b"/>
-            <polygon points="22.35 2.35 18.9 5.22 36.15 5.22 39.6 2.35 22.35 2.35" fill="#f7931e" stroke="#1a1a1a" stroke-linecap="round" stroke-linejoin="round" stroke-width="0.86"/>
-            <polygon points="36.15 26.5 39.6 23.63 39.6 2.35 36.15 5.22 36.15 26.5" fill="#f2f2f2" stroke="#1a1a1a" stroke-linecap="round" stroke-linejoin="round" stroke-width="0.86"/>
-            <line x1="37.88" y1="5.97" x2="37.88" y2="23.05" fill="#fff" stroke="#1a1a1a" stroke-linecap="round" stroke-linejoin="round" stroke-width="0.58"/>
-            <rect x="14.76" y="8.56" width="17.25" height="21.28" stroke-width="1.15" stroke="#1a1a1a" stroke-linecap="round" stroke-linejoin="round" fill="#fff"/>
-            <rect x="11.31" y="11.44" width="17.25" height="21.28" stroke-width="0.86" stroke="#1a1a1a" stroke-linecap="round" stroke-linejoin="round" fill="#fbb03b"/>
-            <polygon points="14.76 8.56 11.31 11.44 28.56 11.44 32.01 8.56 14.76 8.56" fill="#f7931e" stroke="#1a1a1a" stroke-linecap="round" stroke-linejoin="round" stroke-width="0.86"/>
-            <polygon points="28.56 32.71 32.01 29.84 32.01 8.56 28.56 11.44 28.56 32.71" fill="#f2f2f2" stroke="#1a1a1a" stroke-linecap="round" stroke-linejoin="round" stroke-width="0.86"/>
-            <line x1="30.29" y1="12.18" x2="30.29" y2="29.26" fill="#fff" stroke="#1a1a1a" stroke-linecap="round" stroke-linejoin="round" stroke-width="0.58"/>
-          </g>
-          <line x1="1.18" y1="12.02" x2="1.18" y2="20.08" fill="#f2f2f2" stroke="#1a1a1a" stroke-linecap="round" stroke-linejoin="round" stroke-width="0.85"/>
-          <line x1="8.31" y1="22.18" x2="8.31" y2="27.84" fill="#f2f2f2" stroke="#1a1a1a" stroke-linecap="round" stroke-linejoin="round" stroke-width="0.87"/>
-          <line x1="8.31" y1="19.61" x2="8.31" y2="17.98" fill="#f2f2f2" stroke="#1a1a1a" stroke-linecap="round" stroke-linejoin="round" stroke-width="0.87"/>
-        </g>
-        </Group> */}
-        {/* <text
-          textAnchor="start"
-          fontSize="1.125em"
-          x={xMax + margin.right - 130}
-          y={yMax}
-          fontWeight="700"
-          fontFamily="Noto Sans TC"
-          fill="#333333"
-        >
-          <tspan dy="-1em">銷售</tspan>
-          <tspan dy="1.125em" dx="-2em">總量</tspan>
-          <tspan dx="0.25em" dy="-0.125em" fontSize="2em">
-            {totalFormated}
-          </tspan>
-        </text> */}
-       {/* <line
-          x1={0}
-          y1={0}
-          x2={0}
-          y2={yMax}
-          stroke="black"
-        /> */}
+        <Group left={0} top={yMax - 200}>
+          <text
+            textAnchor="end"
+            fontSize="64"
+            x={xMax}
+            y={0}
+            fontWeight="700"
+            fontFamily="Noto Sans TC"
+            fill="#333333"
+          >
+            {y}年
+            <tspan dx="-10" fontSize="120" fill="#ffc027" stroke="black" strokeWidth="4"> {quarter}</tspan>
+          </text>
+          <line
+            x1={xMax - 400}
+            y1={30}
+            x2={xMax}
+            y2={30}
+            stroke="#b3b3b3"
+            strokeWidth="3"
+            strokeDasharray="16"
+            strokeLinecap="round"
+          ></line>
+        </Group>
+        <Group left={xMax - 400} top={yMax - 120}>
+          <Champ />
+          <image x="240" y="-20" width="150" href={imgs[nthQ].default} />
+        </Group>
+        
         <RacingAxisTop
           domainMax={domainMax}
           xMax={xMax}
